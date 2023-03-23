9cf8aefcf50e83e858973a128bbe1bcc086bee0b
Melike Beria Ayas <101648732+Beriaamelike@users.noreply.github.com>
2023-03-23 13:47:36 +0200

    Time: 0 ms (100.00%), Space: 42.6 MB (39.76%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+
+
+class Solution {
+    
+
+    public int[] runningSum(int[] nums) {
+
+        int[] array = new int[nums.length];
+        int sum =0;
+        
+        for(int i=0; i<nums.length; i++) {
+            sum += nums[i];
+            array[i] = sum;
+            
+        }
+        return array;
+    }
+
+
+}
+
+
+
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("dizinin eleman sayisini giriniz: ");
+            int N = scan.nextInt();
+
+            int[] array = new int[N];
+
+            for(int i=0; i<N; i++) {
+                array[i] = scan.nextInt();
+            }
+
+            int[] newArray = sol.runningSum(array);
+
+            System.out.println(newArray);
+        }
+    }
+
+}
